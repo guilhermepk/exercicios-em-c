a1ae30a244d888512d11a0665b1fe2b8a242f9dc
Guilherme Prigol Kramer <guilhermeprigolkramer@gmail.com>
2024-03-07 20:09:47 -0300

    funções min e max

--- ex1/main.c
+++ ex1/main.c
@@ -3,17 +3,21 @@ EX1
 Considerando duas pilhas P1 e P2 que contêm números inteiros,
 desenvolva um programa que inclua as seguintes funcionalidades:
 
-- Implemente uma função para verificar se as pilhas P1 e P2 são idênticas.
+- [x] Implemente uma função para verificar se as pilhas P1 e P2 são idênticas.
 
-- Se diferirem, identifique qual das duas pilhas possui maior quantidade de elementos.
+- [x] Se diferirem, identifique qual das duas pilhas possui maior quantidade de elementos.
 
-- Crie uma função que determine e retorne o valor máximo, mínimo e a média dos números contidos em uma determinada pilha.
+- [ ] Crie uma função que determine e retorne o valor
+    - [x] máximo,
+    - [x] mínimo
+    - [ ] e a média 
+    dos números contidos em uma determinada pilha.
 
-- Desenvolva uma função que permita mover elementos da pilha P1 para a pilha P2 (funcionalidade de cópia).
+- [ ] Desenvolva uma função que permita mover elementos da pilha P1 para a pilha P2 (funcionalidade de cópia).
 
-- Elabore uma função que calcule e retorne a quantidade de números ímpares em uma pilha específica.
+- [ ] Elabore uma função que calcule e retorne a quantidade de números ímpares em uma pilha específica.
 
-- Projete uma função que identifique e conte quantos elementos de uma pilha têm valores pares.
+- [ ] Projete uma função que identifique e conte quantos elementos de uma pilha têm valores pares.
 */
 
 #include <stdio.h>
@@ -92,6 +96,43 @@ Pilha *maiorPilha(Pilha *p1, Pilha *p2){
     }
 }
 
+void imprimirIdenticasOuMaior(Pilha *p1, Pilha *p2){
+    if(pilhasIdenticas(p1, p2)){
+        printf("Identicas\n");
+    }else{
+        Pilha *maior = maiorPilha(p1, p2);
+        if(maior == NULL){
+            printf("Nao ha maior pilha\n");
+        }else{
+            (maior == p1) ? printf("P1 eh a maior pilha") : printf("P2 eh a maior pilha");
+        }
+    }
+}
+
+int maiorValorDaPilha(Pilha *pilha){
+    int max = pilha->pElemento[pilha->topo];
+
+    for(int x = 0; x < pilha->topo+1; x++){
+        if(pilha->pElemento[x] > max){
+            max = pilha->pElemento[x];
+        }
+    }
+
+    return max;
+}
+
+int menorValorDaPilha(Pilha *pilha){
+    int min = pilha->pElemento[pilha->topo];
+
+    for(int x = 0; x < pilha->topo+1; x++){
+        if(pilha->pElemento[x] < min){
+            min = pilha->pElemento[x];
+        }
+    }
+
+    return min;
+}
+
 int main(){
     setlocale(LC_ALL,"Portuguese");
     system("cls");
@@ -101,19 +142,14 @@ int main(){
 
     empilhar(p1, 1);
 
-    empilhar(p2, 1);
-    empilhar(p2, 2);
+    empilhar(p2, 30);
+    empilhar(p2, 99);
+    empilhar(p2, 7);
 
-    if(pilhasIdenticas(p1, p2)){
-        printf("Identicasn\n");
-    }else{
-        Pilha *maior = maiorPilha(p1, p2);
-        if(maior == NULL){
-            printf("Nao ha maior pilha\n");
-        }else{
-            (maior == p1) ? printf("P1 eh a maior pilha") : printf("P2 eh a maior pilha");
-        }
-    }
+    imprimirIdenticasOuMaior(p1, p2);
+
+    printf("\n%d eh o maior valor da pilha p2", maiorValorDaPilha(p2));
+    printf("\n%d eh o menor valor da pilha p2", menorValorDaPilha(p2));
 
     return 0;
 }
