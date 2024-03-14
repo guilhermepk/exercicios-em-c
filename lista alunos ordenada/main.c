e0ad742a80d628b36ae28df78e8372876e0469bf
Guilherme Prigol Kramer <guilhermeprigolkramer@gmail.com>
2024-03-14 20:35:14 -0300

    root -> estrutura de dados II

--- lista alunos ordenada/main.c
+++ lista alunos ordenada/main.c
@@ -1,12 +1,3 @@
-/*
-Desenvolva um programa para registrar 5 estudantes, incluindo para cada um
-- o nome
-- e a nota final.
-Guarde esses dados em uma lista duplamente encadeada sem ordenação.
-Posteriormente, o programa deve exibir os nomes dos estudantes que foram aprovados, isto é, aqueles que obtiveram nota final igual ou superior a 7. 
-Caso não haja nenhum estudante aprovado, o programa deve exibir uma notificação específica.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
